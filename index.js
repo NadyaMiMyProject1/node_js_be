2895ceb850bbdbe850ea7f0508f4c7221b8df3b0
Nadezda Michniova <nadezda.michniova@codeacademylt.onmicrosoft.com>
2022-06-04 13:52:32 -0700

    index.js folder formation commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2/promise');
+
+const { dbConfig, port } = require('./config');
+const auth = require('./src/routes/v1/auth');
+const mokiniai = require('./src/routes/v1/mokiniai');
+
+const main = async () => {
+    const app = express();
+    try {
+        const connection = await mysql.createConnection(dbConfig)
+
+        app.use(express.json());
+        app.use(cors());
+        app.mysql = connection;
+
+        app.use('/v1/auth', auth);
+        app.use('/v1/mokiniai', mokiniai);
+       
+        app.get('*', (req, res) => {
+            res.status(404).send({ error: 'Page not found. Call Jurgita!' })
+        });
+
+        app.listen(port, () => {
+            console.log(`Server running on port: ${port}`);
+        });
+    } catch (error) {
+        console.error(error, 'Something wrong with database');
+    }
+
+};
+
+main();
